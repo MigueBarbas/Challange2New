492fcad566e49b5d303d64e59b653508fcf70226
MigueBarbas <B00157812@mytudublin.ie>
2023-12-13 15:10:21 +0000

    added files

---
+++ Challange2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float fireRate = 20.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireRate -= 0.1f;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && fireRate <= 0.0f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            fireRate = 20.0f;
+        }
+    }
+}
